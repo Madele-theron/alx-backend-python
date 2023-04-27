051c4b6a640520eab6af6193e03a98b966269a83
Madele <madele.theron@gmail.com>
2023-04-27 14:08:08 +0200

    add line before function

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """Define a type-annotated function to learn Duck Typing"""
 from typing import List, Tuple, Sequence, Iterable
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Annotate the below functions parameters return appropriate types"""
     return [(i, len(i)) for i in lst]
