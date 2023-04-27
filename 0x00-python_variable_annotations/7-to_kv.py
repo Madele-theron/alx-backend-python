9a05faff29ac21c5c0223e793ca4551a8b74b94c
Madele <madele.theron@gmail.com>
2023-04-27 13:36:00 +0200

    change the calculation of the return statement

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,4 +5,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     """Function that returns a tuple, 1st el =  """
-    return (k, v ** 2)
+    return (k, v * v)
