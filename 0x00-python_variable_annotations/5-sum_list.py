e49cb586a4c94eb5c0ff87e0d9dcd99f64b835c8
Madele <madele.theron@gmail.com>
2023-04-27 13:15:25 +0200

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""A type-annotated function that returns the sum as a float"""
+
+
+def sum_list(input_list: list) -> float:
+    """Function that returns the sum of floats in a list"""
+    return sum(input_list)
