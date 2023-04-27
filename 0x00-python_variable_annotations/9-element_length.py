9f9fd38ba02499f599c38e41efe9a807a13dd87a
Madele <madele.theron@gmail.com>
2023-04-27 14:05:36 +0200

    task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Define a type-annotated function to learn Duck Typing"""
+from typing import List, Tuple
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """Annotate the below functions parameters return appropriate types"""
+    return [(i, len(i)) for i in lst]
