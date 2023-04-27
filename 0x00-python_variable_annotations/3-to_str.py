0f645d908ad5c1db357af0596d3a9a7ab0826c5c
Madele <madele.theron@gmail.com>
2023-04-27 13:07:33 +0200

    task 3

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""A type-annotated function that returns a str representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """Returns string representation of float"""
+    return str(n)
