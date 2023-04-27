e6860c5b9d083ea1e7f3b294d726f2aa68f07792
Madele <madele.theron@gmail.com>
2023-04-27 14:37:35 +0200

    task 12

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""Type Checking - Validate code and fix errors """
+from typing import Tuple, List
+from math import floor
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Return values with the appropriate types """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, floor(3.0))
