9642890bb4edeb27e3a1b4be97b59e7d0b02f3ed
Madele <madele.theron@gmail.com>
2023-04-27 13:05:02 +0200

    task2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""A type-annotated function that returns the floor of a float"""
+import math
+
+
+def floor(n: float) -> int:
+    """Concatenates two strings (str1 and str2)"""
+    return math.floor(n)
