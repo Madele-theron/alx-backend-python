497cacc67524f7c5c301ae6543543887a4c2bad5
Madele <madele.theron@gmail.com>
2023-04-27 13:35:15 +0200

    import Tuple & fix to uppercase

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Define a type-annotated function that returns a tuple"""
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     """Function that returns a tuple, 1st el =  """
     return (k, v ** 2)
