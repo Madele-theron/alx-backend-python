f051be5a08cf7ef4b70f06dadd40ad6792467ad7
Madele <madele.theron@gmail.com>
2023-04-27 14:20:02 +0200

    task 11

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""More involved type annotations"""
+from typing import Union, Sequence, Any, TypeVar, Mapping
+
+# define a type variable T
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """Augmented code with correct duck-typed annotations"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
