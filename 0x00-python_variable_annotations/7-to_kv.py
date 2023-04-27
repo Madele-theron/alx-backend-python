e6b872c41f344b38bfaea7c8530d758da2cdb8f6
Madele <madele.theron@gmail.com>
2023-04-27 13:33:32 +0200

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Define a type-annotated function that returns a tuple"""
+from typing import Union
+
+
+def to_kv(k: str, v: Union[float, int]) -> tuple[str, float]:
+    """Function that returns a tuple, 1st el =  """
+    return (k, v ** 2)
