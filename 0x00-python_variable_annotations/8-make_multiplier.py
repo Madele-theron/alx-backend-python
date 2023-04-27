a17aec568de4c9619503539e7f47d276cfa87169
Madele <madele.theron@gmail.com>
2023-04-27 13:52:09 +0200

    task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Define a type-annotated function that returns a function"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable:
+    """Function that returns a """
+    def mult(x: float) -> float:
+        return x * multiplier
+    return mult
