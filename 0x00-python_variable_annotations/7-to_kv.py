070d8ccc7db5743b271830ced5db47fed8163c42
Madele <madele.theron@gmail.com>
2023-04-27 13:37:11 +0200

    add to doc

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,5 +4,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
-    """Function that returns a tuple, 1st el =  """
+    """Function that returns a tuple, 1-string, 2-float"""
     return (k, v * v)
