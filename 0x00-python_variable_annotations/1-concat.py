82286acdb4e8653ee7485a4ba9838087439453af
Madele <madele.theron@gmail.com>
2023-04-27 12:49:00 +0200

    add line before function

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """A type-annotated function that concatenates two strings"""
 
-def concat(str1: str, str2: str) -> str :
+
+def concat(str1: str, str2: str) -> str:
     """Concatenates two strings (str1 and str2)"""
     return str1 + str2
