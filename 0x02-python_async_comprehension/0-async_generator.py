f4f09c06fba5361ef3bc48b3e62967dd1128b614
Madele <madele.theron@gmail.com>
2023-05-03 21:54:31 +0200

    use underscore instead of i

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -17,6 +17,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     Yields:
         int: A random float between 0 and 10.
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
