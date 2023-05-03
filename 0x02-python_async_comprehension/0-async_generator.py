13a6213d503ad0b96cdbee436be4ba316e13947e
Madele <madele.theron@gmail.com>
2023-05-03 21:57:12 +0200

    use Generator instead of AsyncGenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields a random float between 0 and 10
     every second for 10 iterations.
