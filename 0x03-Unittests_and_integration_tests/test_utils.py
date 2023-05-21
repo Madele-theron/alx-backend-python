329af6bc730757a711cf60eb8e464316756424b1
Madele <madele.theron@gmail.com>
2023-05-21 12:14:49 +0200

    update paramatirized expected error results

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -29,11 +29,11 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(result, expected_result)
 
     @parameterized.expand([
-        ({}, ("a",), "KeyError: 'a'"),
-        ({"a": 1}, ("a", "b"), "KeyError: 'b'"),
+        ({}, ("a",), KeyError('a')),
+        ({"a": 1}, ("a", "b"), KeyError('b')),
     ])
-    def test_access_nested_map_exception(self,
-                                         nested_map, path, expected_error):
+    def test_access_nested_map_exception(
+            self, nested_map, path, expected_error):
         """
         Test that a KeyError is raised with the expected
         error message for each test case.
@@ -44,11 +44,6 @@ class TestAccessNestedMap(unittest.TestCase):
             path (tuple): The path of keys to access value.
             expected_result: The expected error message.
         """
-        # call the access_nested_map function within
-        # the contect manager "assertRaises"
-        # to assert that a KeyError was raised
-        # The assertRaises context manager catches the
-        # exception and allows us to make further assertions on it.
         with self.assertRaises(KeyError) as cm:
             utils.access_nested_map(nested_map, path)
 
