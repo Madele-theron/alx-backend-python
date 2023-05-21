e23fd799d399275eb536411ce7adfbfafcec4623
Madele <madele.theron@gmail.com>
2023-05-21 12:07:16 +0200

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,6 +7,7 @@ import unittest
 import utils
 from parameterized import parameterized, parameterized_class
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """Test case for access_nested_map function"""
     @parameterized.expand([
@@ -17,7 +18,7 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected_result):
         """
         Test the access_nested_map function with various
-        input cases. Decorated with  @parameterized.expand. 
+        input cases. Decorated with  @parameterized.expand.
 
         Args:
             nested_map (dict): Nested dictionary.
@@ -26,3 +27,29 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         result = utils.access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "KeyError: 'a'"),
+        ({"a": 1}, ("a", "b"), "KeyError: 'b'"),
+    ])
+    def test_access_nested_map_exception(self,
+                                         nested_map, path, expected_error):
+        """
+        Test that a KeyError is raised with the expected
+        error message for each test case.
+        Decorated with  @parameterized.expand.
+
+        Args:
+            nested_map (dict): Nested dictionary.
+            path (tuple): The path of keys to access value.
+            expected_result: The expected error message.
+        """
+        # call the access_nested_map function within
+        # the contect manager "assertRaises"
+        # to assert that a KeyError was raised
+        # The assertRaises context manager catches the
+        # exception and allows us to make further assertions on it.
+        with self.assertRaises(KeyError) as cm:
+            utils.access_nested_map(nested_map, path)
+
+        self.assertEqual(str(cm.exception), expected_error)
